ba6591520fd15cb02d389cf23cf7a4f4648ad736
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-01-09 10:52:56 +0530

    chore: clean trpc routers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
 "use client";
 
-import { client } from "./_trpc/client";
-
 export default function Home() {
-  const { mutateAsync, data } = client.test.addTest.useMutation();
-  const handleClick = async () => {
-    await mutateAsync("test");
-  }
-
   return (
     <div>
-      <button onClick={handleClick}>
-        submit
-      </button>
-      {data && <div>{data}</div>}
+      Page
     </div>
   )
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,7 @@
 import { inferReactQueryProcedureOptions } from "@trpc/react-query";
 import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 import { router } from "./trpc";
-import { testRouter } from "./routers/test";
 export const appRouter = router({
-    test: testRouter
 });
 
 export type AppRouter = typeof appRouter;
