7b21b4cf4b941151b752ebeba795af0a47e09499
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-01-12 09:56:18 +0530

    feat: add routes page for super admins

---
+++ src/app/super-admin/routes/page.tsx
@@ -0,0 +1,20 @@
+import { DataTable } from "@/components/tanstack-react-table/data-table";
+import { routeColumns } from "@/lib/columns";
+
+export default function SuperAdminOrganizationsPage() {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <h1 className="text-2xl font-bold">Fleets Management Page </h1>
+      </div>
+      <div className="mt-10">
+        <DataTable
+          columns={routeColumns}
+          data={[]}
+          placeholder="Search Route Name"
+          searchColumnName="routeName"
+        />
+      </div>
+    </>
+  );
+}
--- src/lib/columns.tsx
+++ src/lib/columns.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { Organization, Fleet } from "@prisma/client";
+import type { Organization, Fleet, Route } from "@prisma/client";
 import type { ColumnDef } from "@tanstack/react-table";
 import { Edit, Trash } from "lucide-react";
 import { DataTableColumnHeader } from "@/components/tanstack-react-table/data-table-column-header";
@@ -474,6 +474,162 @@ export const fleetColumns: ColumnDef<Fleet>[] = [
       <div className="min-w-max">{JSON.stringify(row.getValue("ac"))}</div>
     ),
     accessorKey: "ac",
+  },
+  {
+    id: "actions",
+    header: () => (
+      <div className="flex min-w-max items-center justify-center">Actions</div>
+    ),
+    cell: () => (
+      <div className="min-w-max space-x-2">
+        <Button variant="outline" size="icon">
+          <Edit className="h-4 w-4" />
+        </Button>
+        <Button variant="outline" size="icon">
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+    ),
+  },
+];
+
+export const routeColumns: ColumnDef<Route>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    id: "uniqueId",
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("uniqueId")}</div>
+    ),
+    accessorKey: "id",
+    enableHiding: true,
+  },
+  {
+    id: "routeName",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Route Name" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("routeName")}</div>
+    ),
+    accessorKey: "routeName",
+    enableSorting: true,
+    enableHiding: true,
+  },
+  {
+    id: "stops",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Stops" />
+      </div>
+    ),
+    cell: ({ row }) => <div className="min-w-max">{row.getValue("stops")}</div>,
+    accessorKey: "stops",
+  },
+  {
+    id: "passengerCount",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Passenger Count" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("passengerCount")}</div>
+    ),
+    accessorKey: "passengerCount",
+  },
+  {
+    id: "startTime",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Start Time" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("startTime")}</div>
+    ),
+    accessorKey: "startTime",
+  },
+  {
+    id: "endTime",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="End Time" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("endTime")}</div>
+    ),
+    accessorKey: "endTime",
+  },
+  {
+    id: "startPoint",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Start Point" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("startPoint")}</div>
+    ),
+    accessorKey: "startPoint",
+  },
+  {
+    id: "distance",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Distance" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("distance")}</div>
+    ),
+    accessorKey: "distance",
+  },
+  {
+    id: "duration",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Duration" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("duration")}</div>
+    ),
+    accessorKey: "duration",
+  },
+  {
+    id: "driverId",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Driver ID" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("driverId")}</div>
+    ),
+    accessorKey: "driverId",
   },
   {
     id: "actions",
