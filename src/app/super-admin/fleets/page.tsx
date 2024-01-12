456c6c942f2dd6947d1a6e29f6fb5351cbae3a9a
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-01-12 09:50:07 +0530

    feat: add fleets page for super admins

---
+++ src/app/super-admin/fleets/page.tsx
@@ -0,0 +1,22 @@
+import { DataTable } from "@/components/tanstack-react-table/data-table";
+import { fleetType, fleetColumns } from "@/lib/columns";
+
+export default function SuperAdminOrganizationsPage() {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <h1 className="text-2xl font-bold">Fleets Management Page </h1>
+      </div>
+      <div className="mt-10">
+        <DataTable
+          columns={fleetColumns}
+          data={[]}
+          placeholder="Search Fleet Number"
+          searchColumnName="fleetNumber"
+          facetedFilterColumn1="type"
+          facetedFilterColumnOptions1={fleetType}
+        />
+      </div>
+    </>
+  );
+}
--- src/lib/columns.tsx
+++ src/lib/columns.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { Organization } from "@prisma/client";
+import type { Organization, Fleet } from "@prisma/client";
 import type { ColumnDef } from "@tanstack/react-table";
 import { Edit, Trash } from "lucide-react";
 import { DataTableColumnHeader } from "@/components/tanstack-react-table/data-table-column-header";
@@ -255,6 +255,225 @@ export const orgColumns: ColumnDef<Organization>[] = [
       <div className="min-w-max">{row.getValue("pincode")}</div>
     ),
     accessorKey: "pincode",
+  },
+  {
+    id: "actions",
+    header: () => (
+      <div className="flex min-w-max items-center justify-center">Actions</div>
+    ),
+    cell: () => (
+      <div className="min-w-max space-x-2">
+        <Button variant="outline" size="icon">
+          <Edit className="h-4 w-4" />
+        </Button>
+        <Button variant="outline" size="icon">
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+    ),
+  },
+];
+
+export const statusType: filterType = [
+  { label: "ACTIVE", value: "ACTIVE" },
+  { label: "INACTIVE", value: "INACTIVE" },
+  { label: "MAINTANENCE", value: "MAINTANENCE" },
+  { label: "IDLE", value: "IDLE" },
+];
+
+export const fleetType: filterType = [
+  { label: "BUS", value: "BUS" },
+  { label: "MINI_BUS", value: "MINI_BUS" },
+  { label: "VAN", value: "VAN" },
+];
+
+export const fleetColumns: ColumnDef<Fleet>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    id: "uniqueId",
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("uniqueId")}</div>
+    ),
+    accessorKey: "id",
+    enableHiding: true,
+  },
+  {
+    id: "fleetNumber",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Fleet Number" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("fleetNumber")}</div>
+    ),
+    accessorKey: "fleetNumber",
+    enableSorting: true,
+    enableHiding: true,
+  },
+  {
+    id: "regNumber",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Registration Number" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("regNumber")}</div>
+    ),
+    accessorKey: "regNumber",
+  },
+  {
+    id: "make",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Make" />
+      </div>
+    ),
+    cell: ({ row }) => <div className="min-w-max">{row.getValue("make")}</div>,
+    accessorKey: "make",
+  },
+  {
+    id: "model",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Model" />
+      </div>
+    ),
+    cell: ({ row }) => <div className="min-w-max">{row.getValue("model")}</div>,
+    accessorKey: "model",
+  },
+  {
+    id: "year",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Year" />
+      </div>
+    ),
+    cell: ({ row }) => <div className="min-w-max">{row.getValue("year")}</div>,
+    accessorKey: "year",
+  },
+  {
+    id: "type",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Type" />
+      </div>
+    ),
+    cell: ({ row }) => {
+      const type = fleetType.find(
+        (type) => type.value === row.getValue("type"),
+      );
+
+      if (!type) {
+        return null;
+      }
+
+      return (
+        <div className="min-w-max">
+          <span>{type.label}</span>
+        </div>
+      );
+    },
+    filterFn: (row, id: string, value: Array<string>) => {
+      return value.includes(row.getValue(id)) as boolean;
+    },
+    accessorKey: "type",
+  },
+  {
+    id: "color",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Color" />
+      </div>
+    ),
+    cell: ({ row }) => <div className="min-w-max">{row.getValue("color")}</div>,
+    accessorKey: "color",
+  },
+  {
+    id: "status",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Status" />
+      </div>
+    ),
+    cell: ({ row }) => {
+      const type = statusType.find(
+        (type) => type.value === row.getValue("status"),
+      );
+
+      if (!type) {
+        return null;
+      }
+
+      return (
+        <div className="min-w-max">
+          <span>{type.label}</span>
+        </div>
+      );
+    },
+    filterFn: (row, id: string, value: Array<string>) => {
+      return value.includes(row.getValue(id)) as boolean;
+    },
+    accessorKey: "status",
+  },
+  {
+    id: "fcExpDate",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="FC Expiry Date" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">
+        {new Date(row.getValue("fcExpDate")).toDateString()}
+      </div>
+    ),
+    accessorKey: "fcExpDate",
+  },
+  {
+    id: "capacity",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="Capacity" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">{row.getValue("capacity")}</div>
+    ),
+    accessorKey: "capacity",
+  },
+  {
+    id: "ac",
+    header: ({ column }) => (
+      <div>
+        <DataTableColumnHeader column={column} title="AC" />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="min-w-max">{JSON.stringify(row.getValue("ac"))}</div>
+    ),
+    accessorKey: "ac",
   },
   {
     id: "actions",
